bddf2f36b92fa8a6ccb90bd2eee2c46114bf392a
Anastasia <anastasia@courseshark.com>
2018-03-25 14:49:54 -0700

    Update cnn_multiclass_predict.py

--- cnn_multiclass_predict.py
+++ cnn_multiclass_predict.py
@@ -4,16 +4,16 @@ from keras.models import Sequential
 from keras.models import load_model
 import h5py
 
-classifier = load_model('03_23_18_140PM_my_model_multiclass10.h5') #load the model that was created using cnn_multiclass.py
+classifier = load_model('my_model_multiclass10.h5') #load the model that was created using cnn_multiclass.py
 
-test_image = image.load_img('predictions/pose5.jpg', target_size = (64, 64)) #folder predictions with jpgs that I want to test
+test_image = image.load_img('predictions/pose5.jpg', target_size = (64, 64)) #folder predictions with images that I want to test
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
  
 result = classifier.predict(test_image) # returns array
 
 if result[0][0] == 1:
-	prediction = 'bridge'
+	prediction = 'bridge' #predictions in array are in alphabetical order
 elif result[0][1] == 1:
 	prediction = 'childspose'
 elif result[0][2] == 1:
